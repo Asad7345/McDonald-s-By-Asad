26eb103d60bd41cc0def0f3171f8a6fa9e540296
Asad7345 <asad734577@gmail.com>
2024-11-15 08:56:56 +0500

    Add new Things

--- mcdonald/src/app/McDonald's/home/home.component.ts
+++ mcdonald/src/app/McDonald's/home/home.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  carousel = [
+    {imgPath:'../../../assets/Images/Chick-Mac-D.png'},
+    {imgPath:'../../../assets/Images/Happy-Menu-24-D.png'},
+    {imgPath:'../../../assets/Images/Shakespresso-D.png'},
+    {imgPath:'../../../assets/Images/delivery-points-desktop-slider 33.png'}
 
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- mcdonald/src/app/app.module.ts
+++ mcdonald/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './McDonald\'s/header/header.component';
 import { FooterComponent } from './McDonald\'s/footer/footer.component';
 import { AboutourfoodComponent } from './McDonald\'s/aboutourfood/aboutourfood.component';
 import { TrendingNowComponent } from './McDonald\'s/trending-now/trending-now.component';
+import { HomeComponent } from './McDonald\'s/home/home.component';
 
 
 
@@ -18,7 +19,7 @@ import { TrendingNowComponent } from './McDonald\'s/trending-now/trending-now.co
     FooterComponent,
     AboutourfoodComponent,
     TrendingNowComponent,
-    
+    HomeComponent,
    
     
  
